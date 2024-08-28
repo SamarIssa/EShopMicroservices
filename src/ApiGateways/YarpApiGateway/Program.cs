d839a42b1e915e66609e9a885db2bc6cff8658f6
s.essa.ext <s.essa.ext@Tahaluf.ae>
2024-08-28 10:57:55 +0300

    Yarp Gateway & Shopping Web

--- src/ApiGateways/YarpApiGateway/Program.cs
+++ src/ApiGateways/YarpApiGateway/Program.cs
@@ -1,6 +1,23 @@
+﻿using Microsoft.AspNetCore.RateLimiting;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Add Services to container
+
+builder.Services.AddReverseProxy().LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+builder.Services.AddRateLimiter(options =>
+{
+    options.AddFixedWindowLimiter("fixed", opt =>
+    {
+        //فى العشر ثوانى ماينفعش ابعت اكتر من 5 request
+        opt.PermitLimit = 5;
+        opt.Window = TimeSpan.FromSeconds(10);
+       
+    });
+});
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
-
+// Configure the http request pipeline
+app.UseRateLimiter();
+app.MapReverseProxy();
 app.Run();
